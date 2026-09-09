609a6c1887d18795182f16e7ee662412d942cf96
viper6z <oskarbhc@gmail.com>
2026-09-09 23:17:14 +0200

    done

--- internal/controller/tenant_controller.go
+++ internal/controller/tenant_controller.go
@@ -46,6 +46,9 @@ type TenantReconciler struct {
 // +kubebuilder:rbac:groups=platform.viper6z.dev,resources=tenants,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=platform.viper6z.dev,resources=tenants/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=platform.viper6z.dev,resources=tenants/finalizers,verbs=update
+// +kubebuilder:rbac:groups="",resources=namespaces,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=resourcequotas,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=networking.k8s.io,resources=networkpolicies,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
